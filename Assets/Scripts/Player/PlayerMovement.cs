3a96afc37a81f3e44a503002567ffb666f713d1b
NuclearMystic <solarcompost@gmail.com>
2025-01-24 19:01:00 -0500

    feat: level and character controller
    setup the game play area and the movement controls for the player

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Searcher.SearcherWindow.Alignment;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -24,7 +25,24 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        HandleMovement();
+        if (isRunning)
+        {
+            HandleMovement(runSpeed);
+            Debug.Log("Speed is run speed.");
+        }
+        else if (isSprinting)
+        {
+            HandleMovement(sprintSpeed);
+            Debug.Log("Speed is sprint speed.");
+
+        }
+        else 
+        { 
+            HandleMovement(walkSpeed);
+            Debug.Log("Speed is walk speed.");
+
+        }
+
         HandleRunningToggle();
     }
 
@@ -33,11 +51,30 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.CapsLock))
         {
             isRunning = !isRunning;
+            Debug.Log("Walk/Run toggled.");
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            isSprinting = true;
+            Debug.Log("Sprinting");
+        }
+        else
+        {
+            isSprinting = false;
         }
     }
 
-    private void HandleMovement()
+    private void HandleMovement(float currentSpeed)
     {
-        throw new NotImplementedException();
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 moveVector = transform.right * x + transform.forward * y * currentSpeed;
+        moveVector = moveVector.normalized * currentSpeed;
+
+        Debug.Log("Current speed is " + currentSpeed);
+        characterController.Move(moveVector * Time.deltaTime);
+
     }
 }
---
+++ Assets/WarningLight.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WarningLight : MonoBehaviour
+{
+    public Material warningMaterial;
+    public Material safeMaterial;
+
+    public GameObject warningLight;
+    public GameObject safeLight;
+
+
+
+    private void Update()
+    {
+        
+    }
+}
